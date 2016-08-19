849a14bbb8c143e9f585dc8859bfcb1242eab696
Kevin VanEvery <kvanever@gmail.com>
2016-08-19 16:29:19 -0700

    add enqueue functions for bootstrap and fonts

---
+++ wp-content/themes/red-sweater-project/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function rsp_theme_styles() {
+
+  wp_enqueue_style( 'bootstrap_css', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+
+  wp_enqueue_style( 'google_fonts', "https://fonts.googleapis.com/css?family=Amatic+SC:700|Source+Sans+Pro:400,300,400italic,300italic,700,600,600italic,700italic,900)");
+
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
+
+}
+add_action( 'wp_enqueue_scripts', 'rsp_theme_styles' );
+
+function rsp_theme_js() {
+
+  wp_enqueue_script( 'bootstrap_js', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", array('jquery'), '', false);
+
+  wp_enqueue_script( 'typekit_fonts', "https://use.typekit.net/rbv5wsv.js", '', '', false);
+
+}
+add_action( 'wp_enqueue_scripts', 'rsp_theme_js' );
+
+ ?>
