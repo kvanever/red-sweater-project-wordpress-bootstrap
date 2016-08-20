21cee1ae7aed56fa37da892346eec50069e7eefa
Kevin VanEvery <kvanever@gmail.com>
2016-08-20 16:58:28 -0700

    add spacing for mobile view with pull helper classes

--- wp-content/themes/red-sweater-project/header.php
+++ wp-content/themes/red-sweater-project/header.php
@@ -9,17 +9,13 @@
   </head>
   <body>
     <nav class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">Brand</a>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
-            <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
-            <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
-            <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
-            <li class=""><a href="/donate">Donate</a></li>
-            <li class=""><img src="/images/menu_icon.svg" alt="menu icon" /></li>
-          </ul>
-        </div>
-      </div>
+      <a class="navbar-brand" href="#">Brand</a>
+      <ul class="nav navbar-nav navbar-right pull-right">
+        <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
+        <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
+        <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
+        <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
+        <li class="pull-left"><a href="/donate">Donate</a></li>
+        <li class="pull-left"><img src="/images/menu_icon.svg" alt="menu icon" /></li>
+      </ul>
     </nav>
