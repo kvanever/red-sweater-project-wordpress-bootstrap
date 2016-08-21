d3f4674ed7543c3ed1f577fb9d84617cf66bbdc1
Kevin VanEvery <kvanever@gmail.com>
2016-08-20 18:37:10 -0700

    add svg support for images in header

--- wp-content/themes/red-sweater-project/header.php
+++ wp-content/themes/red-sweater-project/header.php
@@ -9,13 +9,17 @@
   </head>
   <body>
     <nav class="navbar navbar-default" role="navigation">
-      <a class="navbar-brand" href="#">Brand</a>
-      <ul class="nav navbar-nav navbar-right pull-right">
-        <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
-        <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
-        <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
-        <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
-        <li class="pull-left"><a href="/donate">Donate</a></li>
-        <li class="pull-left"><img src="/images/menu_icon.svg" alt="menu icon" /></li>
-      </ul>
+      <div class="navbar-left">
+        <a href="#"><img class="nav-logo" src="../wordpress/wp-content/uploads/2016/08/logo_horiz_redG.svg" alt="Red Sweater Project logo" /></a>
+      </div>
+      <div class="pull-right">
+        <ul class="nav navbar-nav">
+          <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
+          <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
+          <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
+          <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
+          <li class="pull-left"><a href="/donate">Donate</a></li>
+          <li class="pull-left"><img class="subnav-toggle" src="../wordpress/wp-content/uploads/2016/08/menu_icon.svg" alt="menu icon" /></li>
+        </ul>
+      </div>
     </nav>
