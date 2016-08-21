dec1d12d9bd79201c510ceaf46d77706c2317444
Kevin VanEvery <kvanever@gmail.com>
2016-08-20 19:17:27 -0700

    refactor donate and menu out of li

--- wp-content/themes/red-sweater-project/header.php
+++ wp-content/themes/red-sweater-project/header.php
@@ -10,16 +10,22 @@
   <body>
     <nav class="navbar navbar-default" role="navigation">
       <div class="navbar-left">
-        <a href="#"><img class="nav-logo" src="../wordpress/wp-content/uploads/2016/08/logo_horiz_redG.svg" alt="Red Sweater Project logo" /></a>
+        <div class="container-fluid">
+          <a href="#"><img class="nav-logo" src="../wordpress/wp-content/uploads/2016/08/logo_horiz_redG.svg" alt="Red Sweater Project logo" /></a>
+        </div>
       </div>
-      <div class="pull-right">
-        <ul class="nav navbar-nav">
-          <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
-          <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
-          <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
-          <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
-          <li class="pull-left"><a href="/donate">Donate</a></li>
-          <li class="pull-left"><img class="subnav-toggle" src="../wordpress/wp-content/uploads/2016/08/menu_icon.svg" alt="menu icon" /></li>
-        </ul>
+      <div class="pull-right hidden-xs">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li class="hidden-xs"><a href="/who-we-are/the-red-sweater">Who We Are</a></li>
+            <li class="hidden-xs"><a href="/what-we-do/the-school">What We Do</a></li>
+            <li class="hidden-xs"><a href="/get-involved/donate">Get Involved</a></li>
+            <li class="hidden-xs"><a href="/sponsor/sponsor-a-student">Sponsor</a></li>
+          </ul>
+          <a href="/donate" class="navbar-text donate">Donate</a>
+          <div class="navbar-text">
+            <img class="subnav-toggle" src="../wordpress/wp-content/uploads/2016/08/menu_icon.svg" alt="menu icon" />
+          </div>
+        </div>
       </div>
     </nav>
