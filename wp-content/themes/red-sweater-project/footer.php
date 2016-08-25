fcdfa5afdd56d56c47b886777f78dadac2a70c8b
Kevin VanEvery <kvanever@gmail.com>
2016-08-25 14:06:55 -0700

    add html to footer

--- wp-content/themes/red-sweater-project/footer.php
+++ wp-content/themes/red-sweater-project/footer.php
@@ -1,6 +1,6 @@
 <footer>
   <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-3 col-sm-4">
       <a href="<?php bloginfo( 'url' ); ?>">
         <img src="../wordpress/wp-content/uploads/2016/08/logo_horiz_white.svg" alt="Red Sweater Project logo" />
       </a>
@@ -8,12 +8,12 @@
       <p>1000 SW Forest Meadows Way</p>
       <p>Lake Oswego, OR 97034</p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 col-sm-4">
       <nav>
 
       </nav>
     </div>
-    <div class="col-md-offset-3 col-md-3">
+    <div class="col-md-offset-3 col-md-3 col-sm-offset-2 col-sm-2">
       <a href="#"><i class="fa fa-facebook"></i></a>
       <a href="#"><i class="fa fa-twitter"></i></a>
     </div>
